57e2b002d53b107480df290b09ac8740a79a09a9
Leo Hofer <leo.hofer@bfh.ch>
2024-12-06 16:43:01 +0100

    local style json file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function Map() {
   useEffect(() => {
     const map = new MapLibreGL.Map({
       container: 'map',
-      style: 'https://tiles.versatiles.org/styles/colorful.json',
+      style: process.env.PUBLIC_URL + '/colorful.json',
       center: [8.5428242, 47.3669311],
       zoom: 13
     });
